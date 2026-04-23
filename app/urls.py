b9ca191f3defc6e735533144c5d326da867c0fb7
rickxyo <pedrinhooldoni@gmail.com>
2026-04-23 16:55:35 -0300

    chore: project baseline

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from cars.views import cars_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cars/', cars_view),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
