330bc2ec120e938a9c296e6e9cf2d28f32af4fbf
rickxyo <pedrinhooldoni@gmail.com>
2026-04-24 10:27:48 -0300

    feat(cars): display car model in template via view context

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': {'model': 'Nivus'} }
+    )
