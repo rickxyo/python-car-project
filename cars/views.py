34bddcc30e1ddbf8281a7f6b7c31d68f979cc393
rickxyo <pedrinhooldoni@gmail.com>
2026-05-13 15:34:05 -0300

    refactor(cars): migrate cars list from View to ListView

--- cars/views.py
+++ cars/views.py
@@ -1,23 +1,25 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
+            
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
     
-class CarsView(View):
-    
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
         if search:
-            cars = Car.objects.filter(model__icontains=search)
+            cars = cars.filter(model__icontains=search)
+        return cars
     
-        return render(
-             request, 
-             'cars.html', 
-             {'cars': cars }
-    )
-
+               
 class NewCarView(View):
     
     def get(self, request):
