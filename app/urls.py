40fb84e863cf2c4e91eb9de90edaafb91a54ffe8
rickxyo <pedrinhooldoni@gmail.com>
2026-05-14 11:19:52 -0300

    feat(cars): create CarDetailView and car_detail template

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarCreateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -11,7 +11,8 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
-    path('new_car/', NewCarCreateView.as_view(), name='new_car')
+    path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail')
 ]
 
 if settings.DEBUG:
