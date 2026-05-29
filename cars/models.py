44c8d554f585da57e9bcb894432de8d4a78026e8
rickxyo <pedrinhooldoni@gmail.com>
2026-05-29 16:37:10 -0300

    feat(cars): add CarInventory model

--- cars/models.py
+++ cars/models.py
@@ -17,6 +17,16 @@ class Car(models.Model):
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
     
-    
     def __str__(self):
         return self.model
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
